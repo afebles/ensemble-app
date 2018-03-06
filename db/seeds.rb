fe72bbfd61c38847afc1f3573395cfa6d9e44a6c
Aron Febles <afeblesborrome@outlook.com>
2018-03-06 17:33:01 +0100

    Seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,26 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-User.where(email: "test-test@test.com").destroy_all
+# User.where(email: "test-test@test.com").destroy_all
 
-user = User.create!(email: "test-test@test.com", password: "password")
+# user = User.create!(email: "test-test@test.com", password: "password")
 
 puts 'Creating 5 fake users...'
 5.times do
   user = User.new(
-    email: Faker::Movie.quote,
-    encrypted_password: Faker::Number.number(10)
-    first_name: Faker::Name.first_name
-    last_name: Faker::Name.last_name
-    birth_date: Faker::Date.birthday(18, 65)
-    city: Faker::Address.city
-    country: Faker::Address.country
-    about: Faker::Overwatch.quote
-    avatar: Faker::Avatar.image("my-own-slug", "50x50")
-    media: Faker::LoremPixel.image
+    email: Faker::Internet.email,
+    password: Faker::Number.number(10),
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    birth_date: Faker::Date.birthday(18, 65),
+    city: Faker::Address.city,
+    country: Faker::Address.country,
+    about: Faker::Overwatch.quote,
+    avatar: Faker::Avatar.image("my-own-slug", "50x50"),
+    media: Faker::LoremPixel.image,
     banner: Faker::LoremPixel.image
   )
 
-  plan.save!
+  user.save!
 end
 puts 'Finished!'
