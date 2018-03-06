6f992c6740ac545e945d6d08effc7ac2f2714ce5
Aron Febles <afeblesborrome@outlook.com>
2018-03-06 16:16:30 +0100

    unnecesary files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+User.where(email: "test-test@test.com").destroy_all
+
+user = User.create!(email: "test-test@test.com", password: "password")
+
+puts 'Creating 5 fake plans...'
+5.times do
+  user = User.new(
+    title: Faker::Movie.quote,
+    description: Faker::Lorem.paragraphs,
+    user_id: user.id,
+    end_time: nil
+  )
+
+  plan.save!
+end
+puts 'Finished!'
