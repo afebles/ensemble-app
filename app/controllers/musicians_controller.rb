06693af1af86d6a193e6e30d8ebdf11e2bf68b86
Nadia Garcia <nadiagarci@hotmail.com>
2018-03-06 15:17:54 +0100

    added musicians controller

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -18,6 +18,7 @@ class MusiciansController < ApplicationController
         infoWindow: { content: render_to_string(partial: "/musicians/map_box", locals: { musician: musician }) }
       }
     end
+
   end
 
   def show
