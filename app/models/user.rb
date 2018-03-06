6bb0912f9541fd752d5a311f43a0405f77fe53d0
Almudena Vera <almudena.vigara@gmail.com>
2018-03-06 13:01:10 +0100

    added column banner to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :participants, dependent: :destroy
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :birth_date, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :birth_date, presence: true
 
 end
