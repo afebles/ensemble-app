ae039cdd3096e859f979b26ebd7c829e9d225240
Almudena Vera <almudena.vigara@gmail.com>
2018-03-07 18:56:49 +0100

    changed form sign up and add banner to the form edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
 
  def configure_permitted_parameters
    # For additional fields in app/views/devise/registrations/new.html.erb
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :avatar_cache, :last_name, :city, :country, :about, :avatar])
-   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :avatar_cache, :last_name, :city, :country, :about, :avatar])
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :avatar_cache, :last_name, :city, :country, :about, :avatar, :banner])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :avatar_cache, :last_name, :city, :country, :about, :avatar, :banner])
 
    # For additional in app/views/devise/registrations/edit.html.erb
    #devise_parameter_sanitizer.permit(:account_update, keys: [:username])
