cb4ccfccf92f4ee48d71fb8bd48c0745e2beb7ff
Davis Lazdins <davislazdins@gmail.com>
2018-03-07 18:57:21 +0100

    created btn connect and blcok

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :musicians, only: [:index, :show]
+  resources :musicians, only: [:index, :show] do
+    member do
+     get "connect", to: "musicians#connect"
+     get "remove", to: "musicians#remove"
+     get "block", to: "musicians#block"
+     get "unblock", to: "musicians#unblock"
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
