ced12e7f94c2af83ab755c7960015057bcb536a0
Aron Febles <afeblesborrome@outlook.com>
2018-03-08 18:25:22 +0100

    Two indexes to index page, search bar was left with location only

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -2,9 +2,13 @@ class MusiciansController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    if params[:query].present?
-      sql_query = "city ILIKE :query"
-      @musicians = User.where(sql_query, query: "%#{params[:query]}%")
+    if params[:location].present?
+      sql_query = "city ILIKE '#{params[:location]}'"
+      #@instrument = Instrument.where(name: params[:skill])
+
+      #@skill = Skill.where(instrument: @instrument)
+      @musicians = User.where(sql_query)
+
     else
       @musicians = User.all
       @projects = Project.all
