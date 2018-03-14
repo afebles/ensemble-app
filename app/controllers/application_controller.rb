3b98cb4a8ae81cad24e6a400a201fb982a0a0609
Aron Febles <afeblesborrome@outlook.com>
2018-03-14 17:22:02 +0100

    Favicon and else setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,18 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
- def configure_permitted_parameters
-   # For additional fields in app/views/devise/registrations/new.html.erb
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :avatar_cache, :last_name, :city, :country, :about, :avatar, :banner, :instrument, :level, :genre, :media])
-   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :avatar_cache, :last_name, :city, :country, :about, :avatar, :banner, :instrument, :level, :genre, :media])
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :avatar_cache, :last_name, :city, :country, :about, :avatar, :banner, :instrument, :level, :genre, :media])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :avatar_cache, :last_name, :city, :country, :about, :avatar, :banner, :instrument, :level, :genre, :media])
 
-   # For additional in app/views/devise/registrations/edit.html.erb
-   #devise_parameter_sanitizer.permit(:account_update, keys: [:username])
- end
+    # For additional in app/views/devise/registrations/edit.html.erb
+    #devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
 end
 
 
